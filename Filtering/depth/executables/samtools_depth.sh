069af6e3b2059b281cb3944209dbd938909bfe7d
lorenalorenzo <lorena.lorenzo.fdez@gmail.com>
2021-04-19 12:03:06 +0200

    path

--- Filtering/depth/executables/samtools_depth.sh
+++ Filtering/depth/executables/samtools_depth.sh
@@ -27,7 +27,7 @@ module load samtools
 #####################################
 
 # Loop of Samtools depth calculations for each sample_bam
-for i in *cat_ref_sorted_rg_rmdup_sorted_indelrealigner.bam
+for i in /mnt/netapp1/Store_csebdjgl/lynx_genome/lynx_data/CatRef_bams/*cat_ref_sorted_rg_rmdup_sorted_indelrealigner.bam
   do
   echo "Calculating depth for $i"
   samtools depth -a -b /home/csic/bie/llf/Felis_catus.100x100kbp.masked.genome.bed \
